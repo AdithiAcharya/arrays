911246a132104446180ce22288f5e313e1016e8b
AdithiAcharya <154326448+AdithiAcharya@users.noreply.github.com>
2025-02-08 14:41:44 +0530

    Create leftsum_rightsum.c

---
+++ leftsumrightsum.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main() {
+    int n, i,in,h = n;
+    printf("Enter the length of array:\n");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter the elements of the array (must be sorted!):\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter the index :\n");
+    scanf("%d", &in);
+    int ls = 0;
+    int rs = 0;
+    for(i=0;i<in;i++){
+        ls = ls + a[i];
+    }
+    printf("Left Sum : %d\n",ls);
+
+    for(i=in+1;i<n;i++){
+        rs = rs + a[i];
+    }
+    printf("Right Sum : %d\n",rs);
+
+    return 0;
+}
