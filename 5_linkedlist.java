7f32612f5d4d5306d01f6ad926053905db096db7
AdithiAcharya <154326448+AdithiAcharya@users.noreply.github.com>
2025-02-12 14:08:29 +0530

    Create 5_linkedlist.java

---
+++ 5_linkedlist.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+class Main {
+    public static void main(String[] args) {
+        
+        LinkedList<String> l=new LinkedList<String>();
+        
+        l.add("one");
+        l.add("two");
+        l.add("three");
+        
+        System.out.println(l+" ");
+    }
+}
